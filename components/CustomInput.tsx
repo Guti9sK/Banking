fac353bf498f82f6983e5b285d38b8dfd63ee118
Agustín <bestwolf03@gmail.com>
2024-05-16 19:16:19 -0300

    implement auth, appwrite, and sentry

---
+++ components/CustomInput.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { FormControl, FormField, FormLabel, FormMessage } from "./ui/form";
+import { Input } from "./ui/input";
+import { Control, FieldPath, Form } from "react-hook-form";
+import { authFormSchema } from "@/lib/utils";
+import { z } from "zod";
+
+const formSchema = authFormSchema('sign-up');
+
+ interface CustomInputProps{
+    name: FieldPath<z.infer<typeof formSchema>>;
+    label: string;
+    placeholder: string;
+    type: string;
+    control: Control<z.infer<typeof formSchema>>;
+  }
+
+const CustomInput = ({control, name, label, placeholder, type}: CustomInputProps) => {
+  return (
+    <FormField
+                control={control}
+                name={name}
+                render={({ field }) => (
+                  <div className="form-item">
+                        <FormLabel className="form-label">
+                            {label}
+                        </FormLabel>
+                        <div className="flex w-full flex-col">
+                            <FormControl>
+                                <Input 
+                                type={type}
+                                placeholder={placeholder} className="input-class" {...field} />
+                                
+                            </FormControl>
+                            <FormMessage className="form-message mt-2" />
+                        </div>
+                  </div>
+                )}
+              />
+  );
+};
+
+export default CustomInput;
